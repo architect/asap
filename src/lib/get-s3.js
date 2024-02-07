1bafeb12988c6acc2a03ae699497081ba38fe89e
Ryan Block <rblock@gmail.com>
2024-02-07 12:06:21 -0800

    Import plugin

--- src/lib/get-s3.js
+++ src/lib/get-s3.js
@@ -7,7 +7,7 @@ module.exports = async function getS3 () {
   let { s3 } = await awsLite({
     region: process.env.AWS_REGION || 'us-west-2',
     // eslint-disable-next-line
-    plugins: [ require('@aws-lite/s3') ],
+    plugins: [ import('@aws-lite/s3') ],
   })
   _s3 = s3.GetObject
   return _s3
